6bdd9b00cc00f514c9627804e1327eded034269b
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-24 20:26:19 -0500

    Change structure class

--- 2024-1/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
+++ 2024-1/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
@@ -1,8 +1,17 @@
-public class FootballTeam {
+public class FootballTeam implements Comparable<FootballTeam> {
+    private final int gasmesWon;
     public FootballTeam(int gamesWon) {
+        if (gamesWon < 0) {
+            throw new IllegalArgumentException("Not possible to have taken negative games won, you cheater! Go of");
+        }
+        this.gasmesWon = gamesWon;
     }
     public int getGamesWon() {
-        //return 0;
-        return 3;
+        return  gasmesWon;
+    }
+
+    @Override
+    public int compareTo(FootballTeam otherTeam) {
+      return Integer.compare(gasmesWon, otherTeam.gasmesWon);
     }
 }
