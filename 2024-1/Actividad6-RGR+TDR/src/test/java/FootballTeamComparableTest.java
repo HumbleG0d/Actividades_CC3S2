8ea795c3629f9398bf343bcd16d257c331fa1a56
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-24 20:27:12 -0500

    Add test use interfaz comparable

--- 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamComparableTest.java
+++ 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamComparableTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class FootballTeamComparableTest {
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FootballTeamComparableTest {
+
+private static final int  ANY_NUMBER = 123;
+
+@Test
+  void shouldBePossibleToCompareTeams() {
+    FootballTeam team = new FootballTeam(ANY_NUMBER);
+    assertThat(team).isInstanceOf(Comparable.class);
+  }
+
+  @Test
+  void teamsWithMoreMatchesWonShouldBeGreater() {
+    FootballTeam team_2 = new FootballTeam(2);
+    FootballTeam team_3 = new FootballTeam(3);
+    assertThat(team_3.compareTo(team_2)).isGreaterThan(0);
+  }
+
+  @Test
+  void teamsWithLessMatchesWonShouldBeLesser() {
+    FootballTeam team_2 = new FootballTeam(2);
+    FootballTeam team_3 = new FootballTeam(3);
+    assertThat(team_2.compareTo(team_3)).isLessThan(0);
+  }
+
+  @Test
+  void teamsWithSameNumberOfMatchesWonShouldBeEqual() {
+    FootballTeam teamA = new FootballTeam(2);
+    FootballTeam teamB = new FootballTeam(2);
+    assertThat(teamA.compareTo(teamB)).isEqualTo(0);
+  }
 }
