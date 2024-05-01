16c3bfa2dc78ce3572300192312b289053a1a34b
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-30 23:19:32 -0500

    Add class MoneyTest

--- 2024-1/Actividad1-Testing/src/test/java/org/example/MoneyTest.java
+++ 2024-1/Actividad1-Testing/src/test/java/org/example/MoneyTest.java
@@ -1,11 +1,20 @@
 package org.example;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MoneyTest {
     @Test
     void constructorShouldSetAmountAndCurrency() {
-        // Completa
-    }
+        //Arrange
+        int amount = 10; // Cantidad de dinero
+        String currency = "USD"; // Moneda
 
+        //Act
+        var money = new Money(amount , currency); // Crear objeto Money
+
+        //Assert
+        assertThat(money.getCurrency()).as("Currency should be set").isEqualTo(currency); // Verificar que la moneda sea la misma
+        assertThat(money.getAmount()).as("Amount should be set").isEqualTo(amount); // Verificar que la cantidad sea la misma
+    }
 }
