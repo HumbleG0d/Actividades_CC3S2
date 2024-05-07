69d59e30aaa468b98c4e5705b50f243c2a240bed
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-05-07 09:40:02 -0500

    Add refactoring of the asses method

--- 2024-1/Actividad5-TDD/TDD-3/src/main/java/com/wordz/domain/Score.java
+++ 2024-1/Actividad5-TDD/TDD-3/src/main/java/com/wordz/domain/Score.java
@@ -1,30 +1,43 @@
 package com.wordz.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Score {
     private final String correct;
-
-    private Letter result = Letter.INCORRECT ;
-
+    private final List<Letter> results = new ArrayList<>();
+    private int position;
     public Score(String correct) {
         this.correct = correct;
     }
 
-    public Letter letter() {
-        return result;
+    public Letter letter(int position) {
+        return results.get(position);
     }
 
-    public void assess(int position, String attempt) {
-        // Si la letra es correcta, se mantiene el valor de result.
-        for (int i = position; i < attempt.length(); i++) {
-            if (correct.charAt(i) == attempt.
-                    charAt(i)) {
-                result = Letter.CORRECT;
-            } else if (correct.charAt(i) != attempt.charAt(i) && (i == 4)){
-               result = Letter.PART_CORRECT;
-            } else {
-                result = Letter.INCORRECT;
-                break;
-            }
+    public void assess(String attempt) {
+        for (char current: attempt.toCharArray()) {
+            results.add(scoreFor(current) );
+            position++;
         }
     }
+    private boolean occursInWord(char current) {
+        return
+                correct.contains(String.valueOf(current));
+    }
+    private boolean isCorrectLetter(char
+                                            currentLetter) {
+        return correct.charAt(position) ==
+                currentLetter;
+    }
+    private Letter scoreFor(char current) {
+        if (isCorrectLetter(current)) {
+            return Letter.CORRECT;
+        }
+        if (occursInWord(current)) {
+            return Letter.PART_CORRECT;
+        }
+        return Letter.INCORRECT;
+    }
 }
+
