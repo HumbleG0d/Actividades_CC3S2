354d060ff173c8cda7ada199f331d7cfe37aaed5
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-05-07 09:41:01 -0500

    Add test refactoring

--- 2024-1/Actividad5-TDD/TDD-3/src/test/java/com/wordz/domain/WordTest.java
+++ 2024-1/Actividad5-TDD/TDD-3/src/test/java/com/wordz/domain/WordTest.java
@@ -7,59 +7,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class WordTest {
     @Test
-    // Verificamos que si adivinamos una letra correctamente en la posición correcta, se devuelva CORRECT.
-    void oneCorrectLetter() {
-        //Arrange
-        var word = new Word("A");
-        //Act
-        var score = word.guess("A");
-        //Assert
-        assertThat(score.letter()).isEqualTo(CORRECT);
-    }
-
-    @Test
-    // Verificamos que si adivinamos una letra incorrecta, se devuelva INCORRECT.
     void oneIncorrectLetter() {
-        //Arrange
-        var word = new Word("AC");
-        //Act
-        var score = word.guess("AB");
-        //Assert
-        assertThat(score.letter()).isEqualTo(INCORRECT);
-    }
-
-    @Test
-    // Verificamos que si adivinamos todas las letras correctamente en las posiciones correctas, se devuelva CORRECT para cada letra.
-    void allCorrectLetters() {
-        //Arrange
-        String WORD = "AVOID";
-        var word = new Word(WORD);
-        //Act
-        var score = word.guess(WORD);
-        //Assert
-        assertThat(score.letter()).isEqualTo(CORRECT);
-
+        var word = new Word("A");
+        var score = word.guess("Z");
+        assertScoreForGuess(score, INCORRECT);
     }
     @Test
-    // Verificamos que si no adivinamos todas las letras correctamente en las posiciones correctas, se devuelva INCORRECT para cada letra.
-    void allIncorrectLetters() {
-        //Arrange
-        String WORD = "AVOID";
-        var word = new Word(WORD);
-        //Act
-        var score = word.guess("BEGIN");
-        //Assert
-        assertThat(score.letter()).isEqualTo(INCORRECT);
+    void oneCorrectLetter() {
+        var word = new Word("A");
+        var score = word.guess("A");
+        assertScoreForGuess(score, CORRECT);
     }
-
     @Test
-    void partCorrectLetters() {
-        //Arrange
-        String WORD = "AVOID";
-        var word = new Word(WORD);
-        //Act
-        var score = word.guess("AVOIL");
-        //Assert
-        assertThat(score.letter()).isEqualTo(PART_CORRECT);
+    void secondLetterWrongPosition() {
+        var word = new Word("AR");
+        var score = word.guess("ZA");
+        assertScoreForGuess(score, INCORRECT,
+                PART_CORRECT);
+    }
+    @Test
+    void allScoreCombinations() {
+        var word = new Word("ARI");
+        var score = word.guess("ZAI");
+        assertScoreForGuess(score, INCORRECT,
+                PART_CORRECT,
+                CORRECT);
+    }
+    private void assertScoreForGuess(Score score,
+                                     Letter... expectedScores) {
+        for (int position = 0;
+             position < expectedScores.length;
+             position++) {
+            Letter expected =
+                    expectedScores[position];
+            assertThat(score.letter(position))
+                    .isEqualTo(expected);
+        }
     }
 }
+
