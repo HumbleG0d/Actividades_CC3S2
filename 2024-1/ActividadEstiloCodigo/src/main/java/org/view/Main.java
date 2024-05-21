9ca4b18e7499f682d81e03b4ea24ba1c738b160c
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-05-21 16:09:07 -0500

    Add use calculator in main

--- 2024-1/ActividadEstiloCodigo/src/main/java/org/view/Main.java
+++ 2024-1/ActividadEstiloCodigo/src/main/java/org/view/Main.java
@@ -1,10 +1,14 @@
 package org.view;
 
+import org.view.Exameple2.Calculator;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
 
-
+    Calculator calculator = new Calculator();
+    calculator.add(5);
+    System.out.println(calculator.getResult());
   }
 }
