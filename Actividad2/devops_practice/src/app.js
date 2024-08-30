519b60c9cfd81f0f7a27fbd365b366f3d9b5b28e
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-08-29 22:54:52 -0500

    Add metrics and Delete messague hello wordl

--- Actividad2/devops_practice/src/app.js
+++ Actividad2/devops_practice/src/app.js
@@ -1,8 +1,18 @@
+const client = require('prom-client');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+    try {
+      res.set('Content-Type', client.register.contentType);
+      const metrics = await client.register.metrics(); // Espera la resolución de la promesa
+      res.end(metrics);
+    } catch (error) {
+      res.status(500).end(error.message); // Manejo de errores
+    }
+  });
 
 module.exports = app;
